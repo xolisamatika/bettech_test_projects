c5f462b5d6c0fbc0036b2da6f260350b3d93b6a7
Xolisa <xolisamatika@gmail.com>
2016-06-17 15:03:34 +0200

    index.php modified.

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+$config["dbname"] = "testdb";
+$config["host"] = "localhost";
+$config["db_username"] = "root";
+$config["db_password"] = "root";
+$config["base_url"] = "bettechTestProject.com";
+
+?>
--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <strong>  bettech_test_projects &copy; <?php echo date("Y"); ?> </strong> 
+
+<?php 
+include "config.php";
+
+$servername = "10.0.101.115";
+$username = "root";
+$password = "root";
+$dbname = "testdb";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "INSERT INTO users (username, password, email, join_date) VALUES ('john','1234','john@example.com',NOW());";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
